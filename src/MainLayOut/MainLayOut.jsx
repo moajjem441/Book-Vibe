5ce3d39ff62d47693a05035fe6f31fc1dbc2d656
Moajjem Hossain <moajjem441@gmail.com>
2026-04-12 19:47:29 +0600

    MainLayout somepart changed

--- src/MainLayOut/MainLayOut.jsx
+++ src/MainLayOut/MainLayOut.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/Shared/NavBar/Navbar';
+
+
 import { Outlet } from 'react-router';
 
 const MainLayOut = () => {
@@ -8,9 +10,10 @@ const MainLayOut = () => {
 
             <Navbar></Navbar>
             <Outlet></Outlet>
-            
+
         </div>
     );
 };
+
 
 export default MainLayOut;
