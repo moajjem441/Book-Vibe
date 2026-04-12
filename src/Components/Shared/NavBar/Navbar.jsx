be92d066f70cda8ea629019bf9d7092dc33bd465
Moajjem Hossain <moajjem441@gmail.com>
2026-04-12 16:33:19 +0600

    navbar part done

--- src/Components/Shared/NavBar/Navbar.jsx
+++ src/Components/Shared/NavBar/Navbar.jsx
@@ -39,7 +39,9 @@ const Navbar = () => {
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
       
-          {links}
+
+        {links}
+
     </ul>
   </div>
   <div className="navbar-end ">
