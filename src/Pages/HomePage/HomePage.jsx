95a3ece6a079fe30e9f4cc9da403eb362bbc3886
Moajjem Hossain <moajjem441@gmail.com>
2026-04-13 23:55:12 +0600

    some  changed

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import HomeBanner from '../../Components/Shared/NavBar/HomeBanner/HomeBanner';
 
 const HomePage = () => {
+    
     return (
         <div>
 
             <HomeBanner></HomeBanner>
-            
+
         </div>
     );
 };
